9fe9b30f370564e42fc6796c692e8398aa0e92eb
Florian Merle <florian.david.merle@gmail.com>
2026-03-23 15:51:13 +0100

    [tmux] Show battery

---
+++ scripts/battery.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+bat=$(ls /sys/class/power_supply/ | grep BAT | head -n1)
+
+capacity=$(cat /sys/class/power_supply/$bat/capacity)
+status=$(cat /sys/class/power_supply/$bat/status)
+
+case $((capacity / 10)) in
+  0) icon="󰁺" ;;
+  1) icon="󰁻" ;;
+  2) icon="󰁼" ;;
+  3) icon="󰁽" ;;
+  4) icon="󰁾" ;;
+  5) icon="󰁿" ;;
+  6) icon="󰂀" ;;
+  7) icon="󰂁" ;;
+  8) icon="󰂂" ;;
+  9) icon="󰁹" ;;
+  10) icon="󰁹" ;;
+esac
+
+case "$status" in
+  Charging) dir="↑" ;;
+  Discharging) dir="↓" ;;
+  *) arrow="" ;;
+esac
+
+# using kargs for trimming
+echo "$icon $capacity% $dir" | xargs
