08acd6d0f365bf98f8b820d78ac6915b11b25470
Florian Merle <florian.david.merle@gmail.com>
2026-03-31 23:10:08 +0200

    [i3] clicking cycles between power modes

--- regolith3/i3xrocks/conf.d/power_mode.sh
+++ regolith3/i3xrocks/conf.d/power_mode.sh
@@ -4,15 +4,19 @@ LABEL_COLOR=${label_color:-$(xrescat i3xrocks.label.color "#7B8394")}
 VALUE_FONT=${font:-$(xrescat i3xrocks.value.font "Source Code Pro Medium 13")}
 BUTTON=${button:-}
 
-# Get power mode.
-get_power_mode() {
-    powerprofilesctl get
-}
+# Cycle to the next power mode on click
+if [ "$BUTTON" == "1" ]; then
+    case "$(powerprofilesctl get)" in
+        performance) powerprofilesctl set balanced ;;
+        balanced) powerprofilesctl set power-saver ;;
+        power-saver) powerprofilesctl set performance ;;
+    esac
+fi
 
-POWER_MODE=$(get_power_mode)
+POWER_MODE=$(powerprofilesctl get)
 
 if [ "$POWER_MODE" == "performance" ]; then
-    POWER_MODE_ICON="󱐋"
+    POWER_MODE_ICON=""
 elif [ "$POWER_MODE" == "balanced" ]; then
     POWER_MODE_ICON="󰾅"
 elif [ "$POWER_MODE" == "power-saver" ]; then
