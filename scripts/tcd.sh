a280a08286d2a82b4cd510f31013be01b732b618
Florian Merle <florian.david.merle@gmail.com>
2026-04-02 19:14:40 +0200

    [scripts] fix tcd.sh to work with speical chars in dir names

--- scripts/tcd.sh
+++ scripts/tcd.sh
@@ -4,18 +4,19 @@
 
 path=${1:-$(pwd)}
 basename=$(basename $path)
+session=$(echo "$basename" | tr '.:' '__')
 
 # Check if session exist
-found=$(tmux list-sessions -F '#{session_name}' 2>/dev/null | grep -Fx "$basename")
+found=$(tmux list-sessions -F '#{session_name}' 2>/dev/null | grep -Fx "$session")
 
 if [ -n "$found" ]
 then
-	tmux attach-session -d -t $basename -c $path
+	tmux attach-session -d -t "$session" -c "$path"
 	exit 0
 fi
 
 # Create session
-tmux new-session -d -s $basename
+tmux new-session -d -s "$session"
 
 # Check if a git repository is defined
 repository=$(git status 2>&1 | grep -Fv "not a git repository")
@@ -27,5 +28,5 @@ then
 fi
 
 # Finaly, attach to the session in the 1st pane in the 1st window
-tmux select-pane -t ${session}:1.1
-tmux attach-session -t "$basename" -c "$path"
+tmux select-pane -t "$session":1.1
+tmux attach-session -t "$session" -c "$path"
